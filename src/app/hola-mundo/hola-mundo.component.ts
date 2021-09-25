a7e47bc96124703357902307fdcd0544cdf30c8f
Ariel Martin <arielmartin80@gmail.com>
2021-09-25 15:26:06 -0300

    se agrego /dist en .gitignore

--- src/app/hola-mundo/hola-mundo.component.ts
+++ src/app/hola-mundo/hola-mundo.component.ts
@@ -9,8 +9,8 @@ export class HolaMundoComponent implements OnInit {
 
   title = 'Welcome to Angular'
   subtitle = 'This is a Test'
-  image = "/assets/yo_pc.png"
-  image2 = "/assets/calel.jpg"
+  image = "./assets/yo_pc.png"
+  image2 = "./assets/calel.jpg"
 
   constructor() { }
 
